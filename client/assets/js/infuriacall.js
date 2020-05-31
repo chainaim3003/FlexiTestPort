1be1728601f1880cb813c31c0413463e38a80dc5
Shobit Puri <shobitpuri2@gmail.com>
2020-06-01 03:03:38 +0530

    Ui changes

--- client/assets/js/infuriacall.js
+++ client/assets/js/infuriacall.js
@@ -1,37 +1,38 @@
-import Web3 from 'web3';
+// import Web3 from './node_modules/web3/dist/web3.min.js';
 
-const ipfsClient = require('ipfs-http-client')
-const ipfs = ipfsClient({host:'ipfs.infura.io', port:5001, protocol:'https'})
-const all = require('it-all')
+const ipfsClient = require('ipfs-http-client');
+const ipfs = ipfsClient({host:'ipfs.infura.io', port:5001, protocol:'https'});
+const all = require('it-all');
 
-async componentWillMount() {
-    await this.loadWeb3()
-    await this.loadBlockchainData()
+async function componentWillMount() {
+    await this.loadWeb3();
+    await this.loadBlockchainData();
   }
 
-  async loadWeb3() {
+  async function loadWeb3() {
     if (window.ethereum) {
-      window.web3 = new Web3(window.ethereum)
-      await window.ethereum.enable()
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
     }
     else if (window.web3) {
-      window.web3 = new Web3(window.web3.currentProvider)
+      window.web3 = new Web3(window.web3.currentProvider);
     }
     else {
-      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!');
     }
   }
 
-async loadBlockchainData() {
-    const web3 = window.web3
+async function loadBlockchainData() {
+    const web3 = window.web3;
     // Load account
-    const accounts = await web3.eth.getAccounts()
-    this.setState({ account: accounts[0] })
-    const networkId = await web3.eth.net.getId()
+    const accounts = await web3.eth.getAccounts();
+    this.setState({ account: accounts[0] });
+    const networkId = await web3.eth.net.getId();
 
   }
 
-    console.log("Submitting file to ipfs...")
+function runipfs(){
+    console.log("Submitting file to ipfs...");
     
     const data = JSON.stringify({
         link0: "1001",
@@ -39,10 +40,12 @@ async loadBlockchainData() {
         link2: "10035",
         link3: "https://zoom.us/j/99284229358?pwd=bG1vMzNXOWs4bnEwblpWUDFLNTRUQT09",
         link4:"100043"
-    })
-    const ipfsHash = ipfs.add(data)
-    const arr = await all(ipfsHash)
+    });
+    const ipfsHash = ipfs.add(data);
+    const arr =  all(ipfsHash);
 
-    console.info(arr)
-    const loc = arr[0].cid.string
-    console.log(loc)
+    console.info(arr);
+    const loc = arr[0].cid.string;
+    console.log(loc);
+}
+    
