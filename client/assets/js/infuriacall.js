07c546aa10de03d9062451a4df64709a87703cf9
Shobit Puri <shobitpuri2@gmail.com>
2020-05-31 21:12:45 +0530

    made changes in ui

---
+++ client/assets/js/infuriacall.js
@@ -0,0 +1,48 @@
+import Web3 from 'web3';
+
+const ipfsClient = require('ipfs-http-client')
+const ipfs = ipfsClient({host:'ipfs.infura.io', port:5001, protocol:'https'})
+const all = require('it-all')
+
+async componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  }
+
+async loadBlockchainData() {
+    const web3 = window.web3
+    // Load account
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0] })
+    const networkId = await web3.eth.net.getId()
+
+  }
+
+    console.log("Submitting file to ipfs...")
+    
+    const data = JSON.stringify({
+        link0: "1001",
+        link1: "470023",
+        link2: "10035",
+        link3: "https://zoom.us/j/99284229358?pwd=bG1vMzNXOWs4bnEwblpWUDFLNTRUQT09",
+        link4:"100043"
+    })
+    const ipfsHash = ipfs.add(data)
+    const arr = await all(ipfsHash)
+
+    console.info(arr)
+    const loc = arr[0].cid.string
+    console.log(loc)
